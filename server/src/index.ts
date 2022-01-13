8db8c22a431b0449b53ec3dbddfedfef14678860
Victor Elias <victorgelias@gmail.com>
2022-01-13 18:47:53 -0300

    Add CORS support

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 require('dotenv').config()
 
+import cors from 'cors'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import expressWs from 'express-ws'
 
-const { app } = expressWs(express())
+const app = express()
 app.use(cookieParser())
+
+const allowOrigin = process.env.CORS_ALLOW_ORIGIN
+app.use(!allowOrigin ? cors() : cors({ origin: allowOrigin }))
 
 // must be after the call to expressWs above
 import * as handlers from './handlers'
